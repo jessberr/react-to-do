8d286d2a674a4477f9420e12f12e806c6065434e
Jessica Berrett <jessicaberrett@Jessicas-MacBook-Pro.local>
2018-10-14 11:52:35 -0700

    Added delete button

--- components/ToDo.js
+++ components/ToDo.js
@@ -1,14 +1,16 @@
- import React, { Component } from 'react';
+import React, { Component } from 'react';
 
- class ToDo extends Component {
-   render() {
-     return (
-       <li>
-         <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
-         <span>{ this.props.description }</span>
-       </li>
-     );
-   }
- }
+class ToDo extends Component {
+	render() {
+		return (
+			<li>
 
- export default ToDo;
+         		<input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete }  />
+        		<span>{ this.props.description }</span>
+            <input type="button" value="Delete" onClick={ this.props.deleteTodo } />
+         	</li>
+		);
+	}
+}
+
+export default ToDo;
