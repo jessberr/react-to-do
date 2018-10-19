37ca534320adf4bb227c2b67d15a3d2c2f73fd66
Jessica Berrett <jessicaberrett@Jessicas-MacBook-Pro.local>
2018-10-19 05:09:36 -0700

    push all

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,12 +1,16 @@
- import React, { Component } from 'react';
+import React, { Component } from 'react';
 
- class ToDo extends Component {
-   render() {
-     return (
-       <li> So much to do... </li>
+class ToDo extends Component {
+	render() {
+		return (
+			<li>
 
-     );
-   }
- }
+         		<input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete }  />
+        		<span>{ this.props.description }</span>
+            <input type="button" value="Delete" onClick={ this.props.deleteTodo } />
+         	</li>
+		);
+	}
+}
 
- export default ToDo;
+export default ToDo;
